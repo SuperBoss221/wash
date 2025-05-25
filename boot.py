6461ac3bebbf7d66bfd2e85fa0b66e2ffdc7ecbf
SuperBoss221 <Superbos.211@gmail.com>
2025-05-25 21:21:57 +0700

    OK

---
+++ boot.py
@@ -0,0 +1,24 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+import os
+def check_file_exists(filename):
+    try:
+        os.stat(filename)
+        return True
+    except OSError:
+        return False
+
+file_to_check = 'wash.txt'
+if check_file_exists(file_to_check):
+    #os.remove('wash.py')
+    #os.rename(file_to_check, 'wash.py')
+    print(f"UPDATE WASH")
+
+file_to_check = 'main.txt'
+if check_file_exists(file_to_check):
+    #os.remove('main.py')
+    #os.rename(file_to_check, 'main.py')
+    print(f"UPDATE MAIN")
